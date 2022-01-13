5afb868d287e6b2ef85a08ab0896645af57d30c4
owin66 <78900270+owin66@users.noreply.github.com>
2022-01-13 23:23:54 +0200

    adding bootstrap to className

--- my-react-app/src/Component/counter.jsx
+++ my-react-app/src/Component/counter.jsx
@@ -9,8 +9,8 @@ const Counter = () => {
 
     return (
         <>
-            <span>{formatCount()}</span>
-            <button>+</button>
+            <span className='badge bg-primary m-2'>{formatCount()}</span>
+            <button className='btn btn-primary btn-sm m-2'>+</button>
         </>
     );
 }
