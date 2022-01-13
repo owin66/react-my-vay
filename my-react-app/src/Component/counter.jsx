a8d4a3268973e4baf695e4a8d6399546f2607868
owin66 <78900270+owin66@users.noreply.github.com>
2022-01-13 23:41:07 +0200

    adding event onClick to button

--- my-react-app/src/Component/counter.jsx
+++ my-react-app/src/Component/counter.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 
 const Counter = () => {
-    const count = 0;
+    let count = 0;
     const formatCount = () => {
         return count === 0 ? 'empty' : count;
     }
@@ -12,10 +12,19 @@ const Counter = () => {
     let classes = 'badge m-2 ';
     classes += count === 0 ? 'bg-warning' : 'bg-primary';
 
+    const handleIncrement = () => {
+    count = count + 1;
+        console.log(count);
+    }
+
     return (
         <>
             <span className={getBageClasses()}>{formatCount()}</span>
-            <button className='btn btn-primary btn-sm m-2'>+</button>
+            <button
+                className='btn btn-primary btn-sm m-2'
+                onClick={handleIncrement}>
+                +
+            </button>
         </>
     );
 }
