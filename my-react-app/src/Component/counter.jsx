db4c367795da19d453adccc8e614bd2aa00fc458
owin66 <78900270+owin66@users.noreply.github.com>
2022-01-14 11:55:37 +0200

    rendering

--- my-react-app/src/Component/counter.jsx
+++ my-react-app/src/Component/counter.jsx
@@ -25,15 +25,26 @@ const Counter = () => {
         setTags((prevState) => prevState.filter((tag) => tag !== id))
     }
 
+    const renderTags = () => {
+        return (
+            tags.length !== 0 &&
+            tags.map(tag => (<li key={tag}
+                                 className='btn btn-primary btn-sm m-2'
+                                 onClick={() => handleTagChange(tag)}>
+                    {tag}
+                </li>
+            ))
+        )
+
+    }
+    if(tags.length !==0){
+        return <ul>{renderTags()}</ul>
+    }
+
     return (
         <>
-            <ul>{
-                tags.map(tag => (<li key={tag}
-                                     className='btn btn-primary btn-sm m-2'
-                                     onClick={() => handleTagChange(tag)}>
-                    {tag}
-                </li>))
-            }
+            <ul>
+                {renderTags()}
             </ul>
             <span className={getBageClasses()}>{formatCount()}</span>
             <button
