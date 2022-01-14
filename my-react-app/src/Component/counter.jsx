e260201881206c6ed20bae45b0d02b12a416136c
owin66 <78900270+owin66@users.noreply.github.com>
2022-01-14 11:40:38 +0200

    adding handleTagChange

--- my-react-app/src/Component/counter.jsx
+++ my-react-app/src/Component/counter.jsx
@@ -3,6 +3,7 @@ import React, {useState} from "react";
 
 const Counter = () => {
     const [count, setCount] = useState(0)
+    const [tags, setTags] = useState(['tag1', 'tag2', 'tag3'])
 
 
     const formatCount = () => {
@@ -20,9 +21,20 @@ const Counter = () => {
     const handleDecrement = () => {
         setCount((prevState) => prevState - 1)
     }
+    const handleTagChange = (id) => {
+        setTags((prevState) => prevState.filter((tag) => tag !== id))
+    }
 
     return (
         <>
+            <ul>{
+                tags.map(tag => (<li key={tag}
+                                     className='btn btn-primary btn-sm m-2'
+                                     onClick={() => handleTagChange(tag)}>
+                    {tag}
+                </li>))
+            }
+            </ul>
             <span className={getBageClasses()}>{formatCount()}</span>
             <button
                 className='btn btn-primary btn-sm m-2'
